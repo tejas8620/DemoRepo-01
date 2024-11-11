d1bc3d1e5ca6241d87033c5eec9340ac28832706
teja <teja@gmail.com>
2024-11-11 18:49:55 +0530

    first commit from dev1...

---
+++ ProjectGitRepoDemo01/src/com/nt/service/DivisionService.java
@@ -0,0 +1,12 @@
+package com.nt.service;
+
+public class DivisionService {
+	
+	
+	public double div(double input1, double input2) {
+		
+		double result= input1 / input2;
+		return result;
+	}
+
+}
