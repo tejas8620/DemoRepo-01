e3623c78ee44b6de5dc79efcb7c801b2053a33f7
teja <teja@gmail.com>
2024-11-11 17:56:39 +0530

    second commit.... add multiplication class

---
+++ ProjectGitRepoDemo01/src/com/nt/service/MultiplicationService.java
@@ -0,0 +1,11 @@
+package com.nt.service;
+
+public class MultiplicationService {
+	
+	public double mul(double input1, double input2) {
+		
+		double result = input1 * input2;
+		return result;
+	}
+
+}
