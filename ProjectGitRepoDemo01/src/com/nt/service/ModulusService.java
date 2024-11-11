0b16f21e34072e16b98409f4f44eafff1d3e4a49
teja <teja@gmail.com>
2024-11-11 21:55:09 +0530

    add modulus class from developer...

---
+++ ProjectGitRepoDemo01/src/com/nt/service/ModulusService.java
@@ -0,0 +1,12 @@
+package com.nt.service;
+
+public class ModulusService {
+	
+	
+	public double modulus(double input1, double input2) {
+		
+		double result = input1 % input2;
+		return result;
+	}
+
+}
